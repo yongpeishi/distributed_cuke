1cd9aa5655bef37c4511f213afb68798b909f4eb
yongpeishi <pyong@thoughtworks.com>
2013-07-07 16:41:51 +1000

    run scenario only when there is.

--- test_runner.rb
+++ test_runner.rb
@@ -1,6 +1,9 @@
 require 'rest_client'
 
-
 scenario = RestClient.get('http://localhost:4567/scenario')
 
-puts `cucumber #{scenario} -r features --no-color`
+if scenario.empty?
+  puts "No more scenario to run. yay!"
+else
+  puts `cucumber #{scenario} -r features --no-color`
+end
