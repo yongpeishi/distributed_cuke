8f1f2e73f634dda15a5465a76596c99d38502855
yongpeishi <pyong@thoughtworks.com>
2013-07-07 17:09:05 +1000

    server can get back result

--- work_manager.rb
+++ work_manager.rb
@@ -1,9 +1,18 @@
 output = `cucumber features --dry-run --expand --no-color -r features`
 scenario_lines = output.split("\n").select { |line| !!line.match(/^(\s+)?Scenario:/) }
 scenario_locations = scenario_lines.map { |line| line.split("#")[1].strip }
+results = {}
 
 require 'sinatra'
 
 get '/scenario' do
   scenario_locations.pop
+end
+
+post '/result' do
+  results[ params[:scenario] ] = params[:result]
+end
+
+get '/result' do
+  results.inspect
 end
