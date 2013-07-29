59811af775ae883701670fa919f70978f2179f16
yongpeishi <yongpeishi@gmail.com>
2013-07-29 22:58:07 +1000

    post to diff url based on the exit status.

--- test_runner.rb
+++ test_runner.rb
@@ -2,18 +2,21 @@ require 'rest_client'
 
 def get_scenario
   #RestClient.get('http://192.168.1.6:4567/scenario')
-  RestClient.get('http://localhost:4567/scenario')
+  RestClient.get('http://localhost:1337/scenario')
 end
 
-def post_result scenario, output
-  #RestClient.post('http://192.168.1.6:4567/result', scenario: scenario, output: output)
-  RestClient.post('http://localhost:4567/result', scenario: scenario, output: output)
+def post_passed_result scenario, output
+  RestClient.post('http://localhost:1337/passed', task: scenario, output: 'passed')
+end
+
+def post_failed_result scenario, output
+  RestClient.post('http://localhost:1337/failed', task: scenario, output: output)
 end
 
 while not (scenario = get_scenario).empty?
   output = `cucumber #{scenario} -r features --no-color`
 
-  post_result(scenario, output)
+  $?.to_i == 0 ? post_passed_result(scenario, output) : post_failed_result(scenario, output)
 end
 
 puts "done"
